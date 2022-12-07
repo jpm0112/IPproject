6bdfd2480f6fe029cb8bf061d1c715bebfc28668
jpm0112 <jpm0112@auburn.edu>
2022-12-06 19:21:50 -0600

    working newton conjugate gradient method

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 
-import scipy
+from scipy import optimize
 from problems import *
+import numpy as np
+
+
+initial_guess = [0,0]
+fprime = lambda x: optimize.approx_fprime(x, evaluate_ackley, 0.01)
+root = optimize.minimize(evaluate_ackley, initial_guess, method="Newton-CG", jac = fprime)
+root
+
+res = optimize.minimize(evaluate_ackley, initial_guess, method='Nelder-Mead', tol=1e-6)
 
 
 
 
-
-
+evaluate_ackley(0,1)
+initial_guess[1]
 
 
 
--- problems.py
+++ problems.py
@@ -2,8 +2,10 @@
 import numpy as np
 
 # Ackley function
+
+
 def evaluate_ackley(list):
-    x = list[0]
+    x= list[0]
     y = list[1]
     z1 = -20.0* np.exp(-0.2*np.sqrt(0.5* (x**2 + y**2)))
     z2= - np.exp(0.5* (np.cos(2*np.pi*x) + np.cos(2*np.pi*y)))
