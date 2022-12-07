3cda68378151c6a3934ba9e6138be6d314d19121
jpm0112 <jpm0112@auburn.edu>
2022-12-07 16:46:08 -0600

    tidy

--- functions.py
+++ functions.py
@@ -1,16 +1,20 @@
 import numpy.linalg as la
-import numpy as np
+from scipy import optimize
+import time
 
 
-def newton(function, fprime, fprime2, initial_guesses, steps):
+def newton(fun, initial_guesses, steps):
+    start_time = time.time()
     i = 0
+    fprime = lambda x: optimize.approx_fprime(x, fun, 0.0001)
+    fprime2 = lambda x: optimize.approx_fprime(x, fprime, 0.0001)
     while i <steps:
         x = initial_guesses[-1]
         s = la.solve(fprime2(x), fprime(x))
         next_guess = x - s
         initial_guesses.append(next_guess)
         i = i +1
-    return (function(initial_guesses[-1]))
+    return (fun(initial_guesses[-1]), time.time() - start_time)
 
 
 
--- main.py
+++ main.py
@@ -3,36 +3,27 @@ from scipy import optimize
 from problems import *
 from functions import *
 import numpy as np
-import time
 
-start_time = time.time()
+
+
 fun = evaluate_ackley
 initial_guesses = [np.array([0.00011, 0.0111])]
-fprime = lambda x: optimize.approx_fprime(x, fun, 0.0001)
-fprime2 = lambda x: optimize.approx_fprime(x, fprime, 0.0001)
-solution = newton(fun, fprime, fprime2, initial_guesses, 200)
-solution
-elapsed_time = time.time() - start_time
+res = newton(fun, initial_guesses, 200)
 
 
-start_time = time.time()
 fun = evaluate_bona
 initial_guesses = [np.array([0.00011, 0.0111])]
-fprime = lambda x: optimize.approx_fprime(x, fun, 0.0001)
-fprime2 = lambda x: optimize.approx_fprime(x, fprime, 0.0001)
-solution = newton(fun, fprime, fprime2, initial_guesses, 200)
-solution
-elapsed_time = time.time() - start_time
+res = newton(fun, initial_guesses, 200)
+res
 
 
-start_time = time.time()
 fun = evaluate_threecamel
 initial_guesses = [np.array([0.00011, 0.0111])]
-fprime = lambda x: optimize.approx_fprime(x, fun, 0.0001)
-fprime2 = lambda x: optimize.approx_fprime(x, fprime, 0.0001)
-solution = newton(fun, fprime, fprime2, initial_guesses, 200)
-solution
-elapsed_time = time.time() - start_time
+res = newton(fun, initial_guesses, 200)
+res
+
+
+
 
 
 
