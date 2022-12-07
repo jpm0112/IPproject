4ddec81198bff6ead1950378b382375b9006d0e9
jpm0112 <jpm0112@auburn.edu>
2022-12-07 16:41:32 -0600

    with time

--- main.py
+++ main.py
@@ -3,59 +3,40 @@ from scipy import optimize
 from problems import *
 from functions import *
 import numpy as np
+import time
 
-
+start_time = time.time()
 fun = evaluate_ackley
 initial_guesses = [np.array([0.00011, 0.0111])]
 fprime = lambda x: optimize.approx_fprime(x, fun, 0.0001)
 fprime2 = lambda x: optimize.approx_fprime(x, fprime, 0.0001)
-
 solution = newton(fun, fprime, fprime2, initial_guesses, 200)
 solution
+elapsed_time = time.time() - start_time
 
 
+start_time = time.time()
 fun = evaluate_bona
 initial_guesses = [np.array([0.00011, 0.0111])]
 fprime = lambda x: optimize.approx_fprime(x, fun, 0.0001)
 fprime2 = lambda x: optimize.approx_fprime(x, fprime, 0.0001)
-
 solution = newton(fun, fprime, fprime2, initial_guesses, 200)
 solution
+elapsed_time = time.time() - start_time
 
 
+start_time = time.time()
 fun = evaluate_threecamel
 initial_guesses = [np.array([0.00011, 0.0111])]
 fprime = lambda x: optimize.approx_fprime(x, fun, 0.0001)
 fprime2 = lambda x: optimize.approx_fprime(x, fprime, 0.0001)
-
 solution = newton(fun, fprime, fprime2, initial_guesses, 200)
 solution
+elapsed_time = time.time() - start_time
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-root = optimize.minimize(evaluate_ackley, initial_guesses, method="Newton-CG", jac = fprime)
-res = optimize.minimize(evaluate_ackley, initial_guesses, method='Nelder-Mead', tol=1e-6)
 
 
 
